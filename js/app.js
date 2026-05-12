914cf51992a8191b5af2dd98ff01bb19357aaa77
dani <danibayer2003@yahoo.com>
2026-05-12 13:07:16 +0200

    added display upcoming shape functionality

--- js/app.js
+++ js/app.js
@@ -112,6 +112,7 @@ document.addEventListener("DOMContentLoaded", () => {
       current = theTetrominoes[random][currentRotation];
       currentPosition = 4;
       draw();
+      displayShape();
     }
   }
 
@@ -162,6 +163,32 @@ document.addEventListener("DOMContentLoaded", () => {
     current = theTetrominoes[random][currentRotation];
     // checkRotatedPosition();
     draw();
+  }
+
+  // show upcoming tetromino
+  const displaySquares = document.querySelectorAll(".mini-grid div");
+  const displayWidth = 4;
+  let displayIndex = 0;
+
+  const upNextTetrominoes = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2], //lTetromino
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], //zTetromino
+    [1, displayWidth, displayWidth + 1, displayWidth + 2], //tTetromino
+    [0, 1, displayWidth, displayWidth + 1], //oTetromino
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1], //iTetromino
+  ];
+
+  function displayShape() {
+    //remove any trace of a tetromino form the entire grid
+    displaySquares.forEach((square) => {
+      square.classList.remove("tetromino");
+      square.style.backgroundColor = "";
+    });
+    upNextTetrominoes[nextRandom].forEach((index) => {
+      displaySquares[displayIndex + index].classList.add("tetromino");
+      //   displaySquares[displayIndex + index].style.backgroundColor =
+      //     colors[nextRandom];
+    });
   }
 
   draw();
